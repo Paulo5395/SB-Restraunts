965e0ce30198983f830cfa0773079917b23178d3
Cooper Lender <cooperlender@Coopers-MacBook-Air.local>
2024-02-21 12:48:56 -0800

    dark mode

--- myscript.js
+++ myscript.js
@@ -1,7 +1,23 @@
-$(document).ready(function() {});
+
+$(document).ready(function() {
+
+$('#dark').click(function(){
+	$(".a").css("background-color", "rgb(20,20,20)");
+	
+	});
+	$('#light').click(function(){
+	$(".a").css("background-color", "rgb(255,255,255)");
+	});
+	/*$("button").click(function(){
+	$(body).css("background-color", "black");
+	});*/
+});
 
 $(window).on('load', function() {
+
+
 	$('div').fadeIn("fast");
 	$('p').fadeIn("fast");
 	$('h1').fadeIn("fast");
+	
 });
