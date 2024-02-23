9b2557b3ca27579534315bb63d52f9d1ce3e725c
Paulo5395 <paulolange805@gmail.com>
2024-02-23 12:32:27 -0800

    New pages, removed all dark mode

--- myscript.js
+++ myscript.js
@@ -1,37 +1,6 @@
-
 $(document).ready(function() {
-// if (typeof(Storage) !== "undefined"){
-//  if (localStorage.getItem("dark") == "true")
-//  {
-//  	// $(".theme").css("background-color", "#383c42");
-// 	// $(".theme-card").css("background-color", "#565b61");
-//  }
-//  if (localStorage.getItem("dark") == "false")
-//  {
-//  	// $(".theme").css("background-color", "#ffffff");
-// 	// $(".theme-card").css("background-color", "#ffffff");
-//  }
-// }
-// else{
-//  dark = false;
-// }
 
-$('#dark').click(function(){
-	$(".theme").css("background-color", "#383c42");
-	$(".theme-card").css("background-color", "#565b61")
-	$(".theme").css("color", "white");
-	$(".theme-card").css("color", "white");
-	//localStorage.setItem("dark", "true");
-	});
-$('#light').click(function(){
-	$(".theme").css("background-color", "rgb(255,255,255)");
-	$(".theme").css("color", "black");
-	$(".theme-card").css("background-color", "rgb(255,255,255)");
-	$(".theme-card").css("color", "black");
-	//localStorage.setItem("dark", "false");
-	});
 });
-
 
 $(window).on('load', function() {
 	$('div').fadeIn("fast");
